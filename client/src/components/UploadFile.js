d0b51660a3ccc2df1bc7ec6f12d79268fbce1ba4
Ayengar <mercelalex2@gmail.com>
2023-03-04 03:57:35 +0400

    this path should work

--- client/src/components/UploadFile.js
+++ client/src/components/UploadFile.js
@@ -31,7 +31,7 @@ const UploadFile = ({setUploadStatus, setModelPath}) => {
     
     uppy.on('upload-success', (file, response) => {
         const httpBody = response.body;  
-        setModelPath('https://threed-calculator.onrender.com/uploads/' + httpBody.filename);
+        setModelPath('/opt/render/project/src/server/uploads/' + httpBody.filename);
     })
     
     
