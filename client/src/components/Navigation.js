a1959b456ac9dab688a59e563b6b76d032492b22
Ayengar <mercelalex2@gmail.com>
2023-03-06 22:28:15 +0400

    route change

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,12 +6,12 @@ const Navigation = () => {
     return (
     <Navbar bg="light" variant="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/">3D-calc</Navbar.Brand>          
+          <Navbar.Brand href="https://threed-calculator.onrender.com/">3D-calc</Navbar.Brand>          
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="about">About</Nav.Link>
-              <Nav.Link href="contacts">Contacts</Nav.Link>
+              <Nav.Link href="https://threed-calculator.onrender.com/about">About</Nav.Link>
+              <Nav.Link href="https://threed-calculator.onrender.com/contacts">Contacts</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
