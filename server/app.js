950ce817ed98d1d3027957af218ba9e02ef75ca1
Ayengar <mercelalex2@gmail.com>
2023-03-06 23:02:06 +0400

    server change

--- server/app.js
+++ server/app.js
@@ -47,7 +47,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
