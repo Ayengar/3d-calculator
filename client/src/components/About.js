020a72d53f4d5cc7dd009bd888f5143cd9e30b36
Ayengar <mercelalex2@gmail.com>
2023-03-07 01:03:16 +0400

    trying to solve routing

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,14 +1,22 @@
 import Alert from 'react-bootstrap/Alert';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const About = () =>{
     return(
         <>
             <Alert className="About-Container">
+                <Row>
                 Hello! This is the 3D-print price calculator! To find out how much would it cost to 3D-print your 
                 model - simply load one in .stl format.
-                <img src="https://threed-calculator.onrender.com/images/200w.webp"></img>
+                </Row>
+                <Row>
+                    <Col className='Gif-Holder'>
+                     <img src="https://threed-calculator.onrender.com/images/200w.webp" style={{width:"50%"}}></img>
+                     </Col>
+                </Row>
+              
             </Alert>
-            <img src="https://threed-calculator.onrender.com/images/200w.webp"></img>
         </>
     )
 }
--- server/app.js
+++ server/app.js
@@ -10,10 +10,6 @@ var uploads = require('./routes/uploads');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
 app.use(bodyParser.json())
 
@@ -31,6 +27,11 @@ app.use('/uploads', uploads);
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -45,10 +46,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', '../client/build/index.html'));
 });
 
 module.exports = app;
