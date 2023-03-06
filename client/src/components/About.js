c199d14c33bc783b48a10d903dbd749b3efa2b35
Ayengar <mercelalex2@gmail.com>
2023-03-07 03:57:11 +0400

    rename sheesh

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -11,11 +11,13 @@ const About = () =>{
                 Hello! This is the 3D-print price calculator! To find out how much would it cost to 3D-print your 
                 model - simply load one in .stl format.
                 </Row>
+                <hr />
                 <Row>
                     <Col className='Gif-Holder'>
                      <img src="https://threed-calculator.onrender.com/images/200w.webp" style={{width:"50%"}}></img>
                      </Col>
                 </Row>
+                <hr />
                 <Row>
                 <a href='https://www.thingiverse.com/thing:5868206/files'>
                  You can download free models here! <SiThingiverse style={{fontSize: "larger"}}/>
