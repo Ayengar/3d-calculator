678c79522cd6f0a7f2d82efa42b2550a32f02999
Ayengar <mercelalex2@gmail.com>
2023-03-08 21:49:48 +0400

    improved visibility on mobile

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -3,26 +3,29 @@ import { RiMailSendFill } from "react-icons/ri"
 import { BsGithub } from "react-icons/bs"
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 const Contacts = () =>{
     
     return(        
         <Card className="Contacts-Card">
             <Card.Body>
-                <Row>
-                    <Col>
-                        <div className="Icon-Container">
-                            <a className="Contact-Icon" href="https://t.me/ayengar"> <FaTelegram /> </a>
-                            <a className="Contact-Icon" href="https://github.com/Ayengar"> <BsGithub /> </a>
-                        </div>
-                    </Col>
-                </Row>
-                <Row>
-                <div className="Adress-Container"> 
-                    <RiMailSendFill /> <a href="mailto:ayengar@proton.me">  ayengar@proton.me </a>
-                </div>
-                </Row>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className="Icon-Container">
+                                <a className="Contact-Icon" href="https://t.me/ayengar"> <FaTelegram /> </a>
+                                <a className="Contact-Icon" href="https://github.com/Ayengar"> <BsGithub /> </a>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                    <div className="Adress-Container"> 
+                        <RiMailSendFill /> <a href="mailto:ayengar@proton.me">  ayengar@proton.me </a>
+                    </div>
+                    </Row>
+                </Container>
             </Card.Body>
         </Card>
     );
